e9f851e7215f836221d8a972e8699028f0d958d0
Marcos <marcosenzo89@gmail.com>
2026-05-30 09:38:55 -0300

    fix(whatsmeow): define whatsmeow_channels association and map it in helper

--- app/models/account.rb
+++ app/models/account.rb
@@ -98,6 +98,7 @@ class Account < ApplicationRecord
   has_many :web_widgets, dependent: :destroy_async, class_name: '::Channel::WebWidget'
   has_many :webhooks, dependent: :destroy_async
   has_many :whatsapp_channels, dependent: :destroy_async, class_name: '::Channel::Whatsapp'
+  has_many :whatsmeow_channels, dependent: :destroy_async, class_name: '::Channel::Whatsmeow'
   has_many :working_hours, dependent: :destroy_async
 
   has_one_attached :contacts_export
